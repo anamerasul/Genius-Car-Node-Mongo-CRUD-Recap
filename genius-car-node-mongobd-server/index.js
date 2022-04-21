4f504cd88d5ce65657fb0416892c6583bfa02ed3
anamerasul <anamerasul@gmail.com>
2022-04-22 04:30:16 +0600

    Node, Mongo, React, client, server, Rest, CRUD

---
+++ genius-car-node-mongobd-server/index.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import cors from "cors";
+
+
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+
+// middleware 
+
+app.use(cors())
+
+app.use(express.json())
+
+
+
+
+app.get("/", (req, res) => {
+    res.send(` running my geneus car server`)
+
+});
+
+
+app.listen(port, () => {
+    console.log("Listening to port", port)
+})
+
