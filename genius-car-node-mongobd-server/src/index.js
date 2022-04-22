46008bddfe31bc4e6570f8f2e950cb116dca7ace
anamerasul <anamerasul@gmail.com>
2022-04-22 06:45:45 +0600

    Connect to database with secure password on environment variable

--- genius-car-node-mongobd-server/src/index.js
+++ genius-car-node-mongobd-server/src/index.js
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from "cors";
-
+import { MongoClient, ServerApiVersion, ObjectId } from 'mongodb';
 
 
 
@@ -16,6 +16,33 @@ app.use(cors())
 
 app.use(express.json())
 
+
+
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.xbgux.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     // perform actions on the collection object
+
+// });
+
+const run = async () => {
+    try {
+        await client.connect();
+
+        console.log("db connected");
+        const usercollection = client.db("geniousCar").collection("service");
+
+        // const users = { name: 'mahi', email: 'mahi@gmail.com' }
+        // const result = await usercollection.insertOne(users)
+        // console.log(`inserted with the _id: ${result.insertedId}`)
+
+    } finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
 
 
 
